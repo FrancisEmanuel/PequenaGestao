c120493d9f4917c5a5394fc9d37ef3aedb556ea2
Francis Emanuel <francis@engenhariadesistemas.com>
2018-04-23 17:26:15 -0300

    First commit

---
+++ clientes/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import persons_list
+from .views import persons_new, persons_update, persons_delete
+
+urlpatterns = [
+    path('list/', persons_list, name="person_list"),
+    path('new/', persons_new, name="persons_new"),
+    path('update/<int:id>', persons_update, name="persons_update"),
+    path('delete/<int:id>', persons_delete, name="person_delete"),
+
+]
