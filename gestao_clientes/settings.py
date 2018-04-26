6f84ee357c9851c300110d5e1662605f6c0cf1dc
Francis Emanuel <francis@engenhariadesistemas.com>
2018-04-25 22:55:52 -0300

    personalizacao template

--- gestao_clientes/settings.py
+++ gestao_clientes/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bootstrapform',
     'clientes',
     'home',
 ]
@@ -71,7 +72,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'gestaoclientes.wsgi.application'
+WSGI_APPLICATION = 'gestao_clientes.wsgi.application'
 
 
 # Database
